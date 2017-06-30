606dc67c6f57a6e5fb3e14ff0b7f31861d471a3d
Germano Lichtnow Eichenberg <germanoeich@gmail.com>
2017-06-30 14:31:04 -0300

    First command!

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+import secrets from './secrets.js'
+
+const config = {
+  token: secrets.token
+}
+
+export default config
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+import Eris from 'eris'
+import config from './config'
+
+var bot = new Eris(config.token)
+bot.on('ready', () => {
+  console.log('Ready!')
+})
+bot.on('messageCreate', (msg) => {
+  if (msg.content === 'p!ping') {
+    bot.createMessage(msg.channel.id, 'Pong!')
+  }
+
+  if (msg.content === '<3 bulbabot') {
+    bot.createMessage(msg.channel.id, `${msg.author.mention} Stahp it you :3 <3`)
+  }
+})
+bot.connect()
