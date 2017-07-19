b04fb49bc59323c986e33e5334ffecf803007842
Germano Lichtnow Eichenberg <germanoeich@gmail.com>
2017-07-19 13:44:34 -0300

    Better cache and cache invalidation after 12 hours

--- src/util/cache.js
+++ src/util/cache.js
@@ -1,42 +1,40 @@
 // The general idea is to be able to quickly determinate if an entity is cached in memory
-// by using obj["key"]. If it is, we will loop through the main array to retrieve it. But
-// if it isn't, we can move on without iterating through many entities.
+// by using obj["key"]. If it is, the value will be the ID of the pokemon and we can retrieve it
+// using the same idea on the cache list. This way we can quickly check the cache and return the entity
+// without the need to loop
+
 // Contains the IDs of the pokemon that are cached
 const idsCached = {}
 // Contains the names of the pokemon that are cached
 const namesCached = {}
 
-const pokemonCache = []
+// Actual Pokemon cache
+const pokemonCache = {}
 
+// Checks the cache and returns an id if we have it
 export function isCached (pokemonIdOrName) {
   return namesCached[pokemonIdOrName] || idsCached[pokemonIdOrName]
 }
 
 export function retrieve (pokemonIdOrName) {
-  if (isCached(pokemonIdOrName)) {
-    var ret
-    pokemonCache.some(function (p) {
-      if (p.id == pokemonIdOrName || p.name === pokemonIdOrName) { // eslint-disable-line
-        ret = p
-        // some will break out of the loop if some (any) condition is true
-        return true
-      }
-      // to continue the loop
-      return false
-    }, this)
-    return ret
+  var cacheId = isCached(pokemonIdOrName)
+  if (cacheId) {
+    var cacheEntry = pokemonCache[cacheId]
+
+    // Cache will persist for 12 hours
+    if ((cacheEntry.timeAdded.getTime() + (12 * 60 * 60 * 1000)) > new Date().getTime()) {
+      return cacheEntry.pokeInfo
+    }
   }
   return undefined
 }
 
 export function add (pokemonInfo) {
-  if (!namesCached[pokemonInfo.name] || !idsCached[pokemonInfo.id]) {
-    pokemonCache.push(pokemonInfo)
+  pokemonCache[pokemonInfo.id] = { pokeInfo: pokemonInfo, timeAdded: new Date() }
 
-    // Values don't really matter
-    namesCached[pokemonInfo.name] = 1
-    idsCached[pokemonInfo.id] = 1
-  }
+  // Store the ID which will be used to retrieve from the cache
+  namesCached[pokemonInfo.name] = pokemonInfo.id
+  idsCached[pokemonInfo.id] = pokemonInfo.id
 
   return pokemonInfo
 }
