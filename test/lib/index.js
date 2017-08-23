28b3b1a391ef79bcda7eb550db553f09f868d259
Germano Lichtnow Eichenberg <germanoeich@gmail.com>
2017-08-23 12:56:06 -0300

    Adding more tests

---
+++ test/lib/index.js
@@ -0,0 +1,36 @@
+import chai, { expect } from 'chai'
+import { describe, it } from 'mocha'
+import { KeyCache, TwinKeyCache } from '/lib/cache'
+import dirtyChai from 'dirty-chai'
+
+chai.use(dirtyChai)
+
+describe('Cache test', () => {
+  it('Should exist', async () => {
+    const keyCache = new KeyCache(1)
+    const twinKeyCache = new TwinKeyCache(1)
+
+    expect(keyCache).to.exist()
+    expect(twinKeyCache).to.exist()
+  })
+
+  describe('KeyCache test', () => {
+    it('Should cache items', () => {
+      const keyCache = new KeyCache(1)
+      keyCache.add('key', 1)
+
+      const retrievedValue = keyCache.retrieve('key')
+      expect(retrievedValue).to.be.equal(1)
+    })
+    it('Should implement isCached properly', () => {
+      const keyCache = new KeyCache(1)
+      keyCache.add('key', 1)
+
+      const isCached = keyCache.isCached('key')
+      expect(isCached).to.be.equal(true)
+
+      const notCached = keyCache.isCached('unk key')
+      expect(notCached).to.be.equal(false)
+    })
+  })
+})
