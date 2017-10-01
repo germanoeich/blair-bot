ec69a51dee60eb19d034e4811587b9433f84d8c0
Germano Eichenberg <germanoeich@gmail.com>
2017-10-01 01:05:25 -0300

    Changing kick command to be a class like other commands

--- src/commands/moderation/index.js
+++ src/commands/moderation/index.js
@@ -1,4 +1,4 @@
-import kick from './kick'
+import KickCmd from './kick'
 import RoleCmd from './role'
 import PrefixCmd from './prefix'
 import TextCmd from './text'
@@ -9,9 +9,9 @@ function register (bot) {
   cmds.push(new PrefixCmd(bot))
   cmds.push(new TextCmd(bot))
   cmds.push(new RoleCmd(bot))
+  cmds.push(new KickCmd(bot))
 
   cmds.forEach((cmd) => cmd.register())
-  kick.register(bot)
 }
 
 export default {
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,50 +1,50 @@
 import TargetSelector from './../../lib/util/target-selector'
 import Responder from './../../lib/messages/responder'
+import BaseCommand from './../baseCommand'
 
-const info = {
-  name: 'kick',
-  args: '<Username | Userid | User Mention> [Reason] - target selector capable',
-  description: 'kicks someone from the server'
-}
-
-async function action (msg, args) {
-  const responder = new Responder(msg.channel)
-
-  if (!msg.member.permission.has('kickMembers')) {
-    responder.error('You need the "(kickMembers)" permission for that.').send()
-    return
+export default class KickCmd extends BaseCommand {
+  constructor (bot) {
+    const info = {
+      name: 'kick',
+      usage: '<Username | Userid | User Mention> [Reason] - target selector capable',
+      argsRequired: true,
+      description: 'Kicks someone from the server.',
+      fullDescription: 'Kick someone from the server.',
+      requirements: {
+        permissions: {
+          'kickMembers': true
+        }
+      },
+      permissionMessage: 'You need the "Kick Members" permission.',
+      invalidUsageMessage: 'Specify a target'
+    }
+    super(info, bot)
   }
 
-  const targetSelector = new TargetSelector()
+  async action (msg, args) {
+    const responder = new Responder(msg.channel)
 
-  if (args.length === 0) {
-    responder.error('Specify an user').send()
-    return
+    const targetSelector = new TargetSelector()
+
+    if (args.length === 0) {
+      responder.error('Specify an user').send()
+      return
+    }
+
+    let member = await targetSelector.find(msg, args[0])
+    const reason = args[1] || ''
+    if (!member) {
+      return
+    }
+
+    try {
+      await member.kick(reason)
+      responder.success(`User ${member.user.username}#${member.user.discriminator} ( ${member.user.id} ) was kicked from the server`)
+    } catch (e) {
+      const error = JSON.parse(e.response)
+      responder.error(`Failed with error: ${error.code} - ${error.message} `)
+    }
+
+    responder.send()
   }
-
-  let member = await targetSelector.find(msg, args[0])
-  const reason = args[1] || ''
-  if (!member) {
-    return
-  }
-
-  try {
-    await member.kick(reason)
-    responder.success(`User ${member.user.username}#${member.user.discriminator} ( ${member.user.id} ) was kicked from the server`)
-  } catch (e) {
-    const error = JSON.parse(e.response)
-
-    responder.error(`Failed with error: ${error.code} - ${error.message} `)
-  }
-
-  responder.send()
-}
-
-function register (bot) {
-  bot.registerCommand(info.name, action, info)
-}
-
-export default {
-  info,
-  register
 }
