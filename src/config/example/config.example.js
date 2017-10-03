0e2e58417165963b34c2bad114ff99e707130b95
Germano Eichenberg <germanoeich@gmail.com>
2017-10-03 19:25:57 -0300

    Better config structure

--- src/config/example/config.example.js
+++ src/config/example/config.example.js
@@ -1,3 +1,64 @@
+const config = {
+  token: {
+    production: '<TOKEN>',
+    dev: '<TOKEN>',
+    test: '<TOKEN>'
+  },
+  redis: {
+    dev: {
+      host: 'localhost',
+      port: 6379
+    },
+    test: {
+      host: 'localhost',
+      port: 6379
+    },
+    production: {
+      host: '<HOST>',
+      port: 6379
+    }
+  },
+  sentry: {
+    production: {
+      dsn: '<DSN>',
+      // This is used to track releases on sentry. Don't change this value, it get's replaced
+      // by sentryRelease.sh
+      release: 'RELEASE_REPLACE_TOKEN'
+    },
+    dev: {
+      dsn: false
+    },
+    test: {
+      dsn: false
+    }
+  },
+  lavalink: {
+    production: {
+      hosts: [
+        { host: '<HOST>', port: 80, region: 'us', password: '' }
+      ]
+    },
+    test: {
+      hosts: [
+        { host: 'localhost', port: 80, region: 'us', password: '' }
+      ]
+    },
+    dev: {
+      hosts: [
+        { host: 'localhost', port: 80, region: 'us', password: '' }
+      ]
+    }
+  }
+}
+
+const redis = config.redis[process.env.NODE_ENV]
+const sentry = config.sentry[process.env.NODE_ENV]
+const lavalink = config.lavalink[process.env.NODE_ENV]
+const token = config.token[process.env.NODE_ENV]
+
 export default {
-  token: 'bot_token'
+  redis,
+  sentry,
+  lavalink,
+  token
 }
--- src/data/redis.js
+++ src/data/redis.js
@@ -2,8 +2,7 @@ import config from './../config/config.js'
 import Redis from 'ioredis'
 
 function connect () {
-  const env = process.env.NODE_ENV || 'dev'
-  return new Redis(config.redis[env].port, config.redis[env].host)
+  return new Redis(config.redis.port, config.redis.host)
 }
 
 export default {
