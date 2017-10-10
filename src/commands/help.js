1335b03fb333fede552bf0ecbacf7d7eee5ff8ca
Germano Eichenberg <germanoeich@gmail.com>
2017-10-10 17:30:58 +0000

    Adding help command for specific commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,50 +10,82 @@ export default class HelpCmd extends BaseCommand {
     }
     super(info, bot)
     this.categories = categories
+    this.allCmds = []
+    categories.forEach((c) => {
+      this.allCmds = [ ...this.allCmds, ...c.cmds ]
+    })
   }
 
   async action (msg, args, parsedArgs) {
     const responder = new Responder(msg.channel)
 
-    var embed = {
-      title: 'Help',
-      description: 'Commands by category:',
-      url: 'https://discordapp.com',
-      color: parseInt('815FC0', 16),
-      footer: {
-        icon_url: 'https://cdn.discordapp.com/avatars/358403319523180544/837fafe1f4a2ed5242b1025a18e05d37.png',
-        text: 'Tip: You can use "help -a" to get a more detailed command list'
-      },
-      author: {
-        name: 'Blair',
+    if (parsedArgs._.length === 0) {
+      const embed = {
+        title: 'Help',
+        description: 'Commands by category:',
         url: 'https://blairbot.me',
-        icon_url: 'https://cdn.discordapp.com/avatars/358403319523180544/837fafe1f4a2ed5242b1025a18e05d37.png'
-      },
-      fields: []
-    }
+        color: parseInt('815FC0', 16),
+        footer: {
+          icon_url: 'https://cdn.discordapp.com/avatars/358403319523180544/837fafe1f4a2ed5242b1025a18e05d37.png',
+          text: 'Tip: You can use "help -a" to get a more detailed command list'
+        },
+        author: {
+          name: 'Blair',
+          url: 'https://blairbot.me',
+          icon_url: 'https://cdn.discordapp.com/avatars/358403319523180544/837fafe1f4a2ed5242b1025a18e05d37.png'
+        },
+        fields: []
+      }
 
-    if (parsedArgs.a) {
-      embed.fields = this.categories.filter((element) => !!element.categoryName).map((element) => {
-        return {
-          name: `__${element.categoryName}__`,
-          value: element.cmds.map((cmd) => {
-            return `**${cmd.info.name}${cmd.info.aliases ? ` | ${cmd.info.aliases.join(' | ')}` : ''}** - ${cmd.info.description}\n`
-          }).join('')
-        }
-      })
+      if (parsedArgs.a) {
+        embed.fields = this.categories.filter((element) => !!element.categoryName).map((element) => {
+          return {
+            name: `__${element.categoryName}__`,
+            value: element.cmds.map((cmd) => {
+              return `**${cmd.info.name}${cmd.info.aliases ? ` | ${cmd.info.aliases.join(' | ')}` : ''}** - ${cmd.info.description}\n`
+            }).join('')
+          }
+        })
 
-      embed.footer = undefined
+        embed.footer = undefined
+      } else {
+        embed.fields = this.categories.filter((element) => !!element.categoryName).map((element) => {
+          return {
+            name: element.categoryName,
+            value: element.cmds.map((cmd) => {
+              return `\`${cmd.info.name}\``
+            }).join(', ')
+          }
+        })
+      }
+
+      return responder.embed(embed).send()
     } else {
-      embed.fields = this.categories.filter((element) => !!element.categoryName).map((element) => {
-        return {
-          name: element.categoryName,
-          value: element.cmds.map((cmd) => {
-            return `\`${cmd.info.name}\``
-          }).join(', ')
-        }
-      })
-    }
+      const cmdName = parsedArgs._[0]
+      const command = this.allCmds.find((cmd) => cmd.info.name === cmdName || (cmd.info.aliases && cmd.info.aliases.includes(cmdName)))
 
-    await responder.embed(embed).send()
+      if (!command) {
+        return responder.error(`Command ${cmdName} not found`).ttl(10).send()
+      }
+
+      const embed = {
+        title: `${command.info.name}${command.info.aliases ? ` | ${command.info.aliases.join(' | ')}` : ''}`,
+        description: command.info.fullDescription,
+        color: parseInt('815FC0', 16),
+        author: {
+          name: 'Blair',
+          url: 'https://blairbot.me',
+          icon_url: 'https://cdn.discordapp.com/avatars/358403319523180544/837fafe1f4a2ed5242b1025a18e05d37.png'
+        },
+        fields: [
+          {
+            name: 'Usage',
+            value: command.info.usage
+          }
+        ]
+      }
+
+      return responder.embed(embed).send()
+    }
   }
 }
